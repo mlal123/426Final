3549126ed9e3bcee7065b11cf8b80684daf4b6fc
mlal123 <mangzalal@gmail.com>
2018-12-08 16:45:33 -0500

    initial stuff

--- script.js
+++ script.js
@@ -1,5 +1,52 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", function (event) {
+    var url = "http://comp426.cs.unc.edu:3001";
+    $("#login_form").submit(function (e){
+		var name = $("#login_form")[0].username.value;
+		var pass = $("#login_form")[0].password.value;
+		login(name, pass);
+		e.preventDefault();
+	});
     
+    function login(name, pass){
+		$.ajax({
+			url: url + "/sessions",
+			type: "POST",
+			data: {"user":{
+                    "username": name,
+                    "password": pass
+                }   
+            },
+			success: function(response){
+				console.log("success");
+                makePage();
+			},
+			error: function(err){
+				console.log(err);
+                alert("Passowrd or Username is wrong or you are not registered.");
+			}
+		});
+	}
+    
+    function session(name, pass){
+		$.ajax({
+			url: url + "/users",
+			type: "POST",
+            dataType: "json",
+			data: '{"user": {"username": "mlal124","password": "Dragon12#"}}',
+            contentType: 'application/json',
+			success: function(res){
+				console.log(res);
+			},
+			error: function(err){
+				console.log(err);
+			}
+		});
+	}
 });
+
+function makePage(){
+    $("#login_box").hide();
+    $("#main_box").show();
+}
                           
